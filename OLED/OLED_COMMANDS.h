6dca8be460834deda88c157b23b7a3799b5379b4
StevenJobsVisionQuest <tonjaj@itk-d663.win.ntnu.no>
2017-09-22 13:48:27 +0200

    Create: Oled definitions

---
+++ OLED/OLED_COMMANDS.h
@@ -0,0 +1,29 @@
+/*
+ * OLED_COMMANDS.h
+ *
+ * Created: 22.09.2017 13:30:08
+ *  Author: tonjaj
+ */ 
+
+
+#ifndef OLED_COMMANDS_H_
+#define OLED_COMMANDS_H_
+
+#define ASCII_OFFSET 32
+#define DISPLAY_OFF 0xae
+#define SEGMENT_REMAP 0xa1
+#define 
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+
+
+
+
+#endif /* OLED_COMMANDS_H_ */
